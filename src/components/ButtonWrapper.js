385c1aebda91eb15d686dc60ea328fcfcbad0256
Meagle <mindfulyeon@gmail.com>
2020-08-05 16:24:21 +0900

    [버튼 컴포넌트] 스타일 적용, 공유 링크 추가

---
+++ src/components/ButtonWrapper.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style/ButtonWrapper.css";
+
+const ButtonWrapper = () => {
+  return (
+    <div className="button-wrapper">
+      <div className="sns-icons">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-wrapper"
+          title="Tweet this quote!"
+          href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20person%20who%20says%20it%20cannot%20be%20done%20should%20not%20interrupt%20the%20person%20who%20is%20doing%20it.%22%20Chinese%20Proverb"
+        >
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="icon-wrapper"
+          title="Post this quote in the tumblr!"
+          href="https://www.tumblr.com/login?redirect_to=https%3A%2F%2Fwww.tumblr.com%2Fwidgets%2Fshare%2Ftool%3Fposttype%3Dquote%26tags%3Dquotes%252Cfreecodecamp%26caption%3DChinese%2BProverb%26content%3DThe%2Bperson%2Bwho%2Bsays%2Bit%2Bcannot%2Bbe%2Bdone%2Bshould%2Bnot%2Binterrupt%2Bthe%2Bperson%2Bwho%2Bis%2Bdoing%2Bit.%26canonicalUrl%3Dhttps%253A%252F%252Fwww.tumblr.com%252Fbuttons%26shareSource%3Dtumblr_share_button"
+        >
+          <i class="fab fa-tumblr"></i>
+        </a>
+      </div>
+      <div className="new-quote">New quote</div>
+    </div>
+  );
+};
+
+export default ButtonWrapper;
