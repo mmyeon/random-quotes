0204f92ff44f2dff5a96ca9ff26a6c05b43b26bc
Meagle <mindfulyeon@gmail.com>
2020-08-05 13:01:59 +0900

    최상위 컴포넌트 생성 및 배경컬러 적용

---
+++ src/components/RandomQuote.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style/RandomQuote.css";
+
+const RandomQuote = () => {
+  return <div className="random-quote">this is random-quotes-machine</div>;
+};
+
+export default RandomQuote;
