dcc6eed99b54c8bab14aff20d49b2424711380ce
Meagle <mindfulyeon@gmail.com>
2020-08-06 16:14:26 +0900

    프롭스 전달하기

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -3,11 +3,23 @@ import "./style/QuoteBox.css";
 import TextWrapper from "./TextWrapper";
 import ButtonWrapper from "./ButtonWrapper";
 
-const QuoteBox = () => {
+const QuoteBox = (props) => {
+  const quote = props.quote;
+  const author = props.author;
+  const handleClick = props.handleClick;
+  const backgroundColor = props.backgroundColor;
+
   return (
     <div className="quote-box">
-      <TextWrapper />
-      <ButtonWrapper />
+      <TextWrapper
+        quote={quote}
+        author={author}
+        backgroundColor={backgroundColor}
+      />
+      <ButtonWrapper
+        handleClick={handleClick}
+        backgroundColor={backgroundColor}
+      />
     </div>
   );
 };
