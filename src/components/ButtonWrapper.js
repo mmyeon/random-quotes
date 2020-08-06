9c3593bf6b2dc1256628106030bc236b2607e48d
Meagle <mindfulyeon@gmail.com>
2020-08-06 16:14:05 +0900

    [클릭 이벤트 다루기] 클릭하면 문구 바뀌도록 설정 (부모에게 전달받은 함수 자식이 호출함)

--- src/components/ButtonWrapper.js
+++ src/components/ButtonWrapper.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./style/ButtonWrapper.css";
 
-const ButtonWrapper = () => {
+const ButtonWrapper = (props) => {
+  const handleClick = props.handleClick;
+  const backgroundColor = props.backgroundColor;
+
+  function buttonClick() {
+    handleClick();
+  }
+
   return (
     <div className="button-wrapper">
       <div className="sns-icons">
@@ -11,6 +18,7 @@ const ButtonWrapper = () => {
           className="icon-wrapper"
           title="Tweet this quote!"
           href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22The%20person%20who%20says%20it%20cannot%20be%20done%20should%20not%20interrupt%20the%20person%20who%20is%20doing%20it.%22%20Chinese%20Proverb"
+          style={{ background: backgroundColor }}
         >
           <i class="fab fa-twitter"></i>
         </a>
@@ -20,11 +28,18 @@ const ButtonWrapper = () => {
           className="icon-wrapper"
           title="Post this quote in the tumblr!"
           href="https://www.tumblr.com/login?redirect_to=https%3A%2F%2Fwww.tumblr.com%2Fwidgets%2Fshare%2Ftool%3Fposttype%3Dquote%26tags%3Dquotes%252Cfreecodecamp%26caption%3DChinese%2BProverb%26content%3DThe%2Bperson%2Bwho%2Bsays%2Bit%2Bcannot%2Bbe%2Bdone%2Bshould%2Bnot%2Binterrupt%2Bthe%2Bperson%2Bwho%2Bis%2Bdoing%2Bit.%26canonicalUrl%3Dhttps%253A%252F%252Fwww.tumblr.com%252Fbuttons%26shareSource%3Dtumblr_share_button"
+          style={{ background: backgroundColor }}
         >
           <i class="fab fa-tumblr"></i>
         </a>
       </div>
-      <div className="new-quote">New quote</div>
+      <div
+        className="new-quote"
+        onClick={buttonClick}
+        style={{ background: backgroundColor }}
+      >
+        New quote
+      </div>
     </div>
   );
 };
