2ae50b54b405f4b67d0fcf67d083a0287e33c79e
Meagle <mindfulyeon@gmail.com>
2020-08-06 19:53:09 +0900

    [리팩토링] 컴포넌트 이름에서 Wrapper빼기

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./style/QuoteBox.css";
-import TextWrapper from "./TextWrapper";
-import ButtonWrapper from "./ButtonWrapper";
+import Text from "./Text";
+import Button from "./Button";
 
 const QuoteBox = (props) => {
+  // TODO: props 비구조화 할당으로 받기
   const quote = props.quote;
   const author = props.author;
   const handleClick = props.handleClick;
@@ -11,15 +12,10 @@ const QuoteBox = (props) => {
 
   return (
     <div className="quote-box">
-      <TextWrapper
-        quote={quote}
-        author={author}
-        backgroundColor={backgroundColor}
-      />
-      <ButtonWrapper
-        handleClick={handleClick}
-        backgroundColor={backgroundColor}
-      />
+      {/* TODO: 아래 컴포넌트 두개 QuoteBox폴더 내부로 넣기 */}
+      {/* TODO: style 폴더 위치 옮기기 */}
+      <Text quote={quote} author={author} backgroundColor={backgroundColor} />
+      <Button handleClick={handleClick} backgroundColor={backgroundColor} />
     </div>
   );
 };
