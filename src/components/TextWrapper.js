7bdf7553caad1c1f084b84d146c4254fdb44aeb1
Meagle <mindfulyeon@gmail.com>
2020-08-06 16:12:17 +0900

    최상위 컴포넌트에서 컬러 props로 전달하도록 설정

--- src/components/TextWrapper.js
+++ src/components/TextWrapper.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./style/TextWrapper.css";
 
-const TextWrapper = () => {
+const TextWrapper = (props) => {
+  const quote = props.quote;
+  const author = props.author;
+  const backgroundColor = props.backgroundColor;
+
   return (
-    <div className="text-wrapper">
+    <div className="text-wrapper" style={{ color: backgroundColor }}>
       <div className="text">
         <i className="fas fa-quote-left quote-icon"></i>
-        The mind is everything. What you think you become.
+        {quote}
       </div>
-      <span className="author">- Buddha</span>
+      <span className="author">{author}</span>
     </div>
   );
 };
